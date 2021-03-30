302a5a8110ae9e90190fb415df0991a0208270f0
Brendan Dagys <brendandagys@gmail.com>
2021-03-29 22:24:56 -0400

    Added nodemon, concurrently, and dotenv.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express()
 
@@ -14,4 +17,9 @@ app.get('/api/purchases', (req, res) => {
   ])
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}...`)
+)
