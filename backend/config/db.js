8ff0a8b0e4337e844f6b4b65a0c217cc32adaf51
Brendan Dagys <brendandagys@gmail.com>
2021-03-29 22:45:48 -0400

    Connected to MongoDB databases and added colors package.

---
+++ backend/config/db.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(
+      process.env.NODE_ENV === 'production'
+        ? process.env.MONGO_URI_PROD
+        : process.env.MONGO_URI_DEV,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }
+    )
+
+    console.log(
+      `MongoDB connected: ${connection.connection.host}`.cyan.underline
+    )
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold)
+    process.exit(1) // 1 means exit with failure
+  }
+}
+
+export default connectDB
