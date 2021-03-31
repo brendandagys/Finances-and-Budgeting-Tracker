32e28709d7ab7e111bfd6e7244526cce511a6c71
Brendan Dagys <brendandagys@gmail.com>
2021-03-30 23:31:10 -0400

    Final fixes for production deploy.

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import configPassport from './config/passport.js'
 import authRoutes from './routes/authRoutes.js'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import path, { dirname } from 'path'
+import path from 'path'
 
 dotenv.config()
 
@@ -37,12 +37,19 @@ app.get('/api/purchases', (req, res) => {
   ])
 })
 
+const __dirname = path.resolve()
+
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets like our main.js or main.css file
-  app.use(express.static('frontend/build'))
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
   // Express will serve up the index.html file if it doesn't recognize the route
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
   })
 }
 
