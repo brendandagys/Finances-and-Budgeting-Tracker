abd64efc4f87f07a64134edcc05b708b5800548f
Brendan Dagys <brendandagys@gmail.com>
2021-04-02 10:12:13 -0400

    PurchaseForm practically finished on front-end.

--- frontend/src/components/PurchaseForm.js
+++ frontend/src/components/PurchaseForm.js
@@ -2,34 +2,15 @@ import React from 'react'
 import { Formik, Form, useField } from 'formik'
 import * as Yup from 'yup'
 
-const MyTextInput = ({ label, ...props }) => {
+const MyInput = ({ label, ...props }) => {
   // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
   // which we can spread on <input>. We can use field meta to show an error
   // message if the field is invalid and it has been touched (i.e. visited)
   const [field, meta] = useField(props)
   return (
-    <div class='form-group'>
+    <div className='form-group'>
       <label htmlFor={props.id || props.name}>{label}</label>
-      <input className='text-input form-control' {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <small className='error'>{meta.error}</small>
-      ) : null}
-    </div>
-  )
-}
-
-const MyCheckbox = ({ children, ...props }) => {
-  // React treats radios and checkbox inputs differently other input types, select, and textarea.
-  // Formik does this too! When you specify `type` to useField(), it will
-  // return the correct bag of props for you -- a `checked` prop will be included
-  // in `field` alongside `name`, `value`, `onChange`, and `onBlur`
-  const [field, meta] = useField({ ...props, type: 'checkbox' })
-  return (
-    <div class='form-group'>
-      <label className='checkbox-input form-control'>
-        <input type='checkbox' {...field} {...props} />
-        {children}
-      </label>
+      <input {...field} {...props} className='form-control' />
       {meta.touched && meta.error ? (
         <small className='error'>{meta.error}</small>
       ) : null}
@@ -40,7 +21,7 @@ const MyCheckbox = ({ children, ...props }) => {
 const MySelect = ({ label, ...props }) => {
   const [field, meta] = useField(props)
   return (
-    <div class='form-group'>
+    <div className='form-group'>
       <label htmlFor={props.id || props.name}>{label}</label>
       <select {...field} {...props} className='form-control' />
       {meta.touched && meta.error ? (
@@ -50,39 +31,56 @@ const MySelect = ({ label, ...props }) => {
   )
 }
 
-// And now we can use these
+const MyTextArea = ({ label, ...props }) => {
+  const [field, meta] = useField(props)
+  return (
+    <div className='form-group'>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <textarea {...field} {...props} className='form-control' />
+      {meta.touched && meta.error ? (
+        <small className='error'>{meta.error}</small>
+      ) : null}
+    </div>
+  )
+}
+
+const getCurrentDate = () => {
+  let today = new Date()
+  let date = today.getDate()
+  let month = today.getMonth() + 1
+  let year = today.getFullYear()
+  if (date < 10) date = `0${date}`
+  if (month < 10) month = `0${month}`
+
+  return `${year}-${month}-${date}`
+}
+
+const getCurrentTime = () =>
+  new Date().toTimeString().split(' ')[0].substring(0, 5)
+
 const SignupForm = () => {
   return (
     <>
       <h1>Enter a Purchase!</h1>
       <Formik
         initialValues={{
-          date: Date().toString(),
-          time: '',
-          category: 'groceries',
-          item: '', // added for our checkbox
-          amount: '', // added for our select
+          date: getCurrentDate(),
+          time: getCurrentTime(),
+          category: '',
+          item: '',
+          amount: '',
+          description: '',
         }}
-        // validationSchema={Yup.object({
-        //   date: Yup.string()
-        //     .max(15, 'Must be 15 characters or less')
-        //     .required('Required'),
-        //   time: Yup.string()
-        //     .max(20, 'Must be 20 characters or less')
-        //     .required('Required'),
-        //   category: Yup.string()
-        //     .email('Invalid email address')
-        //     .required('Required'),
-        //   item: Yup.boolean()
-        //     .required('Required')
-        //     .oneOf([true], 'You must accept the terms and conditions.'),
-        //   amount: Yup.string()
-        //     .oneOf(
-        //       ['designer', 'development', 'product', 'other'],
-        //       'Invalid Job Type'
-        //     )
-        //     .required('Required'),
-        // })}
+        validationSchema={Yup.object({
+          date: Yup.string().required('Required'),
+          time: Yup.string().required('Required'),
+          category: Yup.string().required('Required'),
+          item: Yup.string().required('Required'),
+          amount: Yup.number()
+            .positive('Must be positive')
+            .required('Required'),
+          description: Yup.string().required('Required'),
+        })}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2))
@@ -91,19 +89,9 @@ const SignupForm = () => {
         }}
       >
         <Form>
-          <MyTextInput
-            label='Date'
-            name='date'
-            type='date'
-            placeholder='Date...'
-          />
+          <MyInput label='Date' name='date' type='date' placeholder='Date...' />
 
-          <MyTextInput
-            label='Time'
-            name='time'
-            type='time'
-            placeholder='Time...'
-          />
+          <MyInput label='Time' name='time' type='time' placeholder='Time...' />
 
           <MySelect label='Category' name='category'>
             <option value=''>Select a category</option>
@@ -114,18 +102,32 @@ const SignupForm = () => {
             <option value='clothes'>Clothes</option>
           </MySelect>
 
-          <MyTextInput
+          <MyInput
             label='Item(s)'
             name='item'
             type='text'
             placeholder='Item(s)...'
           />
 
-          <MyTextInput
+          <MyInput
             label='Amount'
             name='amount'
             type='number'
             placeholder='Amount...'
+          />
+
+          <MyTextArea
+            label='Description'
+            name='description'
+            placeholder='Description...'
+            rows='3'
+          />
+
+          <MyInput
+            label='Receipt'
+            name='receipt'
+            type='file'
+            accept='image/png, image/jpeg'
           />
 
           <button className='form-control btn-primary' type='submit'>
