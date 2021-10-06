0e2ac0865399841d3acc743ce423eccb0a6637b8
Brendan Dagys <brendandagys@gmail.com>
2021-10-05 20:00:07 -0400

    Add /finances before auth routes in Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
         return
       case false:
         return (
-          <Navbar.Brand href='/auth/google' className='text-center'>
+          <Navbar.Brand href='/finances/auth/google' className='text-center'>
             <img
               alt='Google Sign In Button'
               src={googleSignInButton}
@@ -23,7 +23,7 @@ const Header = () => {
         )
       default:
         return (
-          <Nav.Link href='/auth/logout' className='text-center'>
+          <Nav.Link href='/finances/auth/logout' className='text-center'>
             <i className='fab fa-google'></i> Log Out
           </Nav.Link>
         )
