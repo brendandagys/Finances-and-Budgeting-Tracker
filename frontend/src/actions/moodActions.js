38d6ae169e78d0d2d11cc61c1def29806f57f5f6
Brendan Dagys <brendandagys@gmail.com>
2021-12-13 19:45:00 -0500

    Revert from using custom `axios.js` module

--- frontend/src/actions/moodActions.js
+++ frontend/src/actions/moodActions.js
@@ -1,4 +1,4 @@
-import axios from '../axios'
+import axios from 'axios'
 
 import {
   MOOD_LIST_REQUEST,
--- frontend/src/components/PurchaseForm.js
+++ frontend/src/components/PurchaseForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
-import axios from '../axios'
+import axios from 'axios'
 import Loader from './Loader'
 import Message from './Message'
 import { Spinner } from 'react-bootstrap'
